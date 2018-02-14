047d2c2fa9a92f0361c7369b849766b584f08d79
imamdigmi <imam.digmi@gmail.com>
2018-02-14 14:50:13 +0000

    Move jwt endpoint

--- core/urls.py
+++ core/urls.py
@@ -15,9 +15,19 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from rest_framework_jwt.views import (
+    obtain_jwt_token,
+    refresh_jwt_token,
+    verify_jwt_token,
+)
 
 urlpatterns = [
     path('', include('peserta.urls')),
     path('tes/', include('tes.urls')),
     path('admin/', admin.site.urls),
+
+    # JWT endpoints
+    path('api/v1/token-auth/', obtain_jwt_token),
+    path('api/v1/token-refresh/', refresh_jwt_token),
+    path('api/v1/token-verify/', verify_jwt_token),
 ]
--- tes/api/urls.py
+++ tes/api/urls.py
@@ -1,9 +1,4 @@
 from django.urls import path
-from rest_framework_jwt.views import (
-    obtain_jwt_token,
-    refresh_jwt_token,
-    verify_jwt_token,
-)
 
 from .views import (
     SoalView,
@@ -12,11 +7,6 @@ from .views import (
 )
 
 urlpatterns = [
-    path('token-auth/', obtain_jwt_token),
-    path('token-refresh/', refresh_jwt_token),
-    path('token-verify/', verify_jwt_token),
-
-    # endpoint
     path('', SoalView.as_view(), name='index'),
     path('<int:pk>/', SoalDetailView.as_view(), name='detail'),
     path('<str:kegiatan>/', SoalKegiatanView.as_view(), name='kegiatan'),
