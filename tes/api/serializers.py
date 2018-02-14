1649ad6c6d87d9ffab3f028d5e20083a961d5c4b
imamdigmi <imam.digmi@gmail.com>
2018-02-14 09:14:27 +0000

    Add api

--- tes/api/serializers.py
+++ tes/api/serializers.py
@@ -4,9 +4,19 @@ from tes.models import Soal, PilihanSoal
 class SoalSerializer(serializers.ModelSerializer):
     class Meta:
         model = Soal
-        fields = (
-            'pk',
-            'soal',
-            'kunci_jawaban',
-        )
+        fields = ('pk', 'kegiatan', 'soal', 'kunci_jawaban',)
+        read_only_fields = ('created_at', 'updated_at')
+
+class PilihanSoalSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = PilihanSoal
+        fields = ('pilihan', 'jawaban',)
+        read_only_fields = ('created_at', 'updated_at')
+
+class SoalDetailSerializer(serializers.ModelSerializer):
+    pilihan = PilihanSoalSerializer(many=True, read_only=True)
+
+    class Meta:
+        model = Soal
+        fields = ('pk', 'kegiatan', 'soal', 'kunci_jawaban', 'pilihan',)
         read_only_fields = ('created_at', 'updated_at')
--- tes/api/views.py
+++ tes/api/views.py
@@ -1,11 +1,27 @@
 from rest_framework import generics
 
-from tes.models import Soal
-from .serializers import SoalSerializer
+from tes.models import (
+    Soal,
+    PilihanSoal,
+    Konfigurasi,
+)
 
-class SoalView(generics.RetrieveAPIView):
-    lookup_field = 'pk'
+from .serializers import (
+    SoalSerializer,
+    SoalDetailSerializer,
+    PilihanSoalSerializer,
+)
+
+class SoalView(generics.ListAPIView):
     serializer_class = SoalSerializer
 
     def get_queryset(self):
         return Soal.objects.all()
+
+class SoalDetailView(generics.RetrieveAPIView):
+    lookup_field = 'pk'
+    serializer_class = SoalDetailSerializer
+
+    def get_queryset(self):
+        pk = self.kwargs['pk']
+        return Soal.objects.filter(pk=pk)
